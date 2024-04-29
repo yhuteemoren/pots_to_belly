22a0e8ce2ae94609fbdab72e3d0fce0d8e034962
yhuteemoren <utibeabasiumoren1@gmail.com>
2024-04-29 16:00:10 +0100

    more style

---
+++ script.js
@@ -0,0 +1,14 @@
+// window.onscroll = function() {stickyNav()};
+
+// var navLinks = document.getElementById("navLinks");
+// var sticky = navLinks.offsetTop;
+
+// function stickyNav() {
+//   if (window.pageYOffset >= sticky) {
+//     navLinks.style.position = "sticky";
+//     navLinks.style.top = "0";
+//   } else {
+//     navLinks.style.position = "absolute";
+//     navLinks.style.top = "50%";
+//   }
+// }
